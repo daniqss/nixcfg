8c9f884665bbad5ad1cfbd468e5ba42731450176
daniqss <danielqueijo14@gmail.com>
2025-06-20 20:47:50 +0200

    add git home manager config fr fr

--- home/git.nix
+++ home/git.nix
@@ -17,15 +17,9 @@ in {
     enable = true;
     userName = "daniqss";
     userEmail = "danielqueijo14@gmail.com";
-    signing = {
-      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB5cDJjedDVYhu7H3WO2cfuIsn9RKjwXE+1+v3fZpc9X danielqueijo14@gmail.com";
-      signByDefault = true;
-    };
 
     extraConfig = {
       core.editor = "nvim";
-      gpg.format = "ssh";
-      gpg.ssh.program = "ssh";
       push.default = "current";
       push.autoSetupRemote = true;
     };
