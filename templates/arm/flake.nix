78e92ab4de81b545e86451e0b9e23e8dc6091523
daniqss <danielqueijo14@gmail.com>
2025-09-08 16:06:14 +0200

    feat(templates): add arm template and expose it

---
+++ templates/arm/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "basic arm template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              gnumake
+              gcc
+
+              gcc-arm-embedded
+              newlib
+            ];
+          };
+      }
+    );
+}
--- templates/default.nix
+++ templates/default.nix
@@ -3,4 +3,12 @@
     path = ./rust;
     description = "basic template for rust";
   };
+  opengl = {
+    path = ./opengl;
+    description = "basic opengl in c template";
+  };
+  arm = {
+    path = ./arm;
+    description = "basic arm template";
+  };
 }
