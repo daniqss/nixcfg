56f4505bf55956e491a6c56d62fc1af34976f1ca
daniqss <danielqueijo14@gmail.com>
2025-11-30 23:47:34 +0100

    stoneward dom 30 nov 2025 23:47:34 CET

--- hosts/common/services/minecraft.nix
+++ hosts/common/services/minecraft.nix
@@ -5,6 +5,11 @@
 }: {
   imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
 
+  # to access with `sudo tmux -S /run/minecraft/mc-gf.sock`
+  environment.systemPackages = with pkgs; [
+    tmux
+  ];
+
   services.minecraft-servers = {
     enable = true;
     eula = true;
@@ -15,26 +20,30 @@
 
       serverProperties = {
         motd = "mc con pauli ";
+        allow-cheats = true;
+        level-name = "mc-gf";
       };
 
-      package = pkgs.fabricServers.fabric-1_21_10.override {
-        loaderVersion = "0.18.1";
-      };
-
-      symlinks = {
-        mods = pkgs.linkFarmFromDrvs "mods" (
-          builtins.attrValues {
-            Fabric-API = pkgs.fetchurl {
-              url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/dQ3p80zK/fabric-api-0.138.3%2B1.21.10.jar";
-              sha512 = "sha512-3HOjZTwplHbR9wy2ksTjWsP2lLOwhz49C3KelS6ZK4eNGo4LHRBJpEKg1IPTBoBzGU8Vr1LqmThURhbiBDPMOA==";
-            };
-            Carpet = pkgs.fetchurl {
-              url = "https://cdn.modrinth.com/data/TQTTVgYE/versions/oiUqSOMA/fabric-carpet-1.21.10-1.4.188%2Bv251016.jar";
-              sha512 = "sha512-36uTeE/KpzsghUhXBjAl02ovfCc5fMZLFB/iyFAkmTqHk2AWrII8LJRfJzFLmTiqV+BrVWdqHDScEhtEXs/iEw==";
-            };
-          }
-        );
-      };
+      package = pkgs.vanillaServers.vanilla-1_21_8;
     };
+    #   package = pkgs.fabricServers.fabric-1_21_10.override {
+    #     loaderVersion = "0.18.1";
+    #   };
+
+    #   symlinks = {
+    #     mods = pkgs.linkFarmFromDrvs "mods" (
+    #       builtins.attrValues {
+    #         Fabric-API = pkgs.fetchurl {
+    #           url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/dQ3p80zK/fabric-api-0.138.3%2B1.21.10.jar";
+    #           sha512 = "sha512-3HOjZTwplHbR9wy2ksTjWsP2lLOwhz49C3KelS6ZK4eNGo4LHRBJpEKg1IPTBoBzGU8Vr1LqmThURhbiBDPMOA==";
+    #         };
+    #         Carpet = pkgs.fetchurl {
+    #           url = "https://cdn.modrinth.com/data/TQTTVgYE/versions/oiUqSOMA/fabric-carpet-1.21.10-1.4.188%2Bv251016.jar";
+    #           sha512 = "sha512-36uTeE/KpzsghUhXBjAl02ovfCc5fMZLFB/iyFAkmTqHk2AWrII8LJRfJzFLmTiqV+BrVWdqHDScEhtEXs/iEw==";
+    #         };
+    #       }
+    #     );
+    #   };
+    # };
   };
 }
--- hosts/stoneward/configuration.nix
+++ hosts/stoneward/configuration.nix
@@ -5,7 +5,7 @@
 }: {
   imports = [
     ./hardware-configuration.nix
-    # ../common/services/minecraft.nix
+    ../common/services/minecraft.nix
     ../common/desktops
     ../common/gaming.nix
     ../common/network.nix
