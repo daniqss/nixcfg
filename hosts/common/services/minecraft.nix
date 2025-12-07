06a02bd6cdb678c3414ca89230e7e7ef45cb9f08
daniqss <danielqueijo14@gmail.com>
2025-12-07 16:49:32 +0100

    feat: fabric works today for some reason

--- hosts/common/services/minecraft.nix
+++ hosts/common/services/minecraft.nix
@@ -5,7 +5,7 @@
 }: {
   imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
 
-  # to access with `sudo tmux -S /run/minecraft/mc-gf.sock`
+  # to access mc console with with `sudo tmux -S /run/minecraft/mc-gf.sock`
   environment.systemPackages = with pkgs; [
     tmux
   ];
@@ -24,26 +24,24 @@
         level-name = "mc-gf";
       };
 
-      package = pkgs.vanillaServers.vanilla-1_21_8;
-    };
-    #   package = pkgs.fabricServers.fabric-1_21_10.override {
-    #     loaderVersion = "0.18.1";
-    #   };
+      package = pkgs.fabricServers.fabric-1_21_10.override {
+        loaderVersion = "0.18.1";
+      };
 
-    #   symlinks = {
-    #     mods = pkgs.linkFarmFromDrvs "mods" (
-    #       builtins.attrValues {
-    #         Fabric-API = pkgs.fetchurl {
-    #           url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/dQ3p80zK/fabric-api-0.138.3%2B1.21.10.jar";
-    #           sha512 = "sha512-3HOjZTwplHbR9wy2ksTjWsP2lLOwhz49C3KelS6ZK4eNGo4LHRBJpEKg1IPTBoBzGU8Vr1LqmThURhbiBDPMOA==";
-    #         };
-    #         Carpet = pkgs.fetchurl {
-    #           url = "https://cdn.modrinth.com/data/TQTTVgYE/versions/oiUqSOMA/fabric-carpet-1.21.10-1.4.188%2Bv251016.jar";
-    #           sha512 = "sha512-36uTeE/KpzsghUhXBjAl02ovfCc5fMZLFB/iyFAkmTqHk2AWrII8LJRfJzFLmTiqV+BrVWdqHDScEhtEXs/iEw==";
-    #         };
-    #       }
-    #     );
-    #   };
-    # };
+      symlinks = {
+        mods = pkgs.linkFarmFromDrvs "mods" (
+          builtins.attrValues {
+            Fabric-API = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/dQ3p80zK/fabric-api-0.138.3%2B1.21.10.jar";
+              sha512 = "sha512-3HOjZTwplHbR9wy2ksTjWsP2lLOwhz49C3KelS6ZK4eNGo4LHRBJpEKg1IPTBoBzGU8Vr1LqmThURhbiBDPMOA==";
+            };
+            Carpet = pkgs.fetchurl {
+              url = "https://cdn.modrinth.com/data/TQTTVgYE/versions/oiUqSOMA/fabric-carpet-1.21.10-1.4.188%2Bv251016.jar";
+              sha512 = "sha512-36uTeE/KpzsghUhXBjAl02ovfCc5fMZLFB/iyFAkmTqHk2AWrII8LJRfJzFLmTiqV+BrVWdqHDScEhtEXs/iEw==";
+            };
+          }
+        );
+      };
+    };
   };
 }
