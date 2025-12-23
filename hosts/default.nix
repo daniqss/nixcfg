cc236d75f91f3ff6d4c20bda5877528427a042ab
daniqss <danielqueijo14@gmail.com>
2025-12-23 20:34:44 +0100

    stoneward mar 23 dic 2025 20:34:44 CET

--- hosts/default.nix
+++ hosts/default.nix
@@ -81,7 +81,7 @@ in {
         imports = with inputs.nixos-raspberrypi.nixosModules; [
           raspberry-pi-5.base
           raspberry-pi-5.page-size-16k
-          raspberry-pi-5.display-vc4
+          # raspberry-pi-5.display-vc4
           raspberry-pi-5.bluetooth
         ];
       })
