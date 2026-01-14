f56ea07b34bb25a7b47d8ce5bab771b9d88def71
daniqss <danielqueijo14@gmail.com>
2026-01-14 13:53:24 +0100

    add trusted users

--- nixos/profiles/common/nix.nix
+++ nixos/profiles/common/nix.nix
@@ -13,6 +13,8 @@
       substituters = ["https://nixos-raspberrypi.cachix.org"];
       trusted-substituters = ["https://nixos-raspberrypi.cachix.org"];
       trusted-public-keys = ["nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="];
+
+      trusted-users = ["root" "@wheel" username];
     };
   };
 
