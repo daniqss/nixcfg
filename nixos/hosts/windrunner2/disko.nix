6aabb5ae573329ed658347bb7fba3a1f9b258fc9
daniqss <danielqueijo14@gmail.com>
2026-03-03 00:27:14 +0100

    fix windrunner disko

--- nixos/hosts/windrunner2/disko.nix
+++ nixos/hosts/windrunner2/disko.nix
@@ -1,44 +1,48 @@
 {...}: {
   disko.devices = {
-    disk = {
-      my-disk = {
-        device = "/dev/sda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "500M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = ["umask=0077"];
-              };
+    disk.nvme0 = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "1024M";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "noatime"
+                "noauto"
+                "x-systemd.automount"
+                "x-systemd.idle-timeout=1min"
+                "umask=0077"
+              ];
             };
+          };
 
-            swap = {
-              label = "SWAP";
-              size = "16G";
-              content = {
-                type = "swap";
-                resumeDevice = true;
-              };
+          swap = {
+            label = "SWAP";
+            size = "16G";
+            content = {
+              type = "swap";
+              resumeDevice = true;
             };
+          };
 
-            root = {
-              label = "ROOT";
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-                mountOptions = [
-                  "noatime"
-                ];
-                extraArgs = ["-O" "encrypt"];
-              };
+          root = {
+            label = "ROOT";
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "noatime"
+              ];
+              extraArgs = ["-O" "encrypt"];
             };
           };
         };
