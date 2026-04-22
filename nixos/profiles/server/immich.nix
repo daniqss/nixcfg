fa4c225ee8f7e40907a8b57b56d6a1013be21a12
daniqss <danielqueijo14@gmail.com>
2026-04-22 10:51:05 +0200

    immich??

--- nixos/profiles/server/immich.nix
+++ nixos/profiles/server/immich.nix
@@ -1,5 +1,6 @@
 {
   config,
+  pkgs,
   lib,
   ...
 }: {
@@ -8,19 +9,27 @@
   config = lib.mkIf config.server.immich.enable {
     services.immich = {
       enable = true;
+      package = pkgs.unstable.immich;
       port = 2283;
+
+      host = "0.0.0.0";
       openFirewall = true;
-      database = {
-        enableVectors = false;
-        enableVectorChord = true;
-      };
     };
 
-    services.immich-public-proxy = {
-      enable = true;
-      port = 2284;
-      openFirewall = true;
-      immichUrl = "http://immich";
+    services.nginx.virtualHosts."immich.example.com" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://[::1]:${toString config.services.immich.port}";
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
+        extraConfig = ''
+          client_max_body_size 50000M;
+          proxy_read_timeout   600s;
+          proxy_send_timeout   600s;
+          send_timeout         600s;
+        '';
+      };
     };
 
     users.users.immich.extraGroups = ["video" "render"];
