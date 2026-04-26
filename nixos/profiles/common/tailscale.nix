5ec96922531bb907d768124e04420a10417c9647
daniqss <danielqueijo14@gmail.com>
2026-04-26 17:43:30 +0200

    mmmm certs 3

--- nixos/profiles/common/tailscale.nix
+++ nixos/profiles/common/tailscale.nix
@@ -17,22 +17,6 @@
   config = lib.mkIf config.common.tailscale.enable {
     services.resolved.enable = true;
 
-    # to allow caddy to read the certs
-    users.groups.tailscale-certs = {};
-
-    systemd.tmpfiles.settings."10-tailscale-certs" = {
-      "/var/lib/tailscale"."z" = {
-        mode = "0751";
-        user = "root";
-        group = "root";
-      };
-      "/var/lib/tailscale/certs"."d" = {
-        mode = "0750";
-        user = "root";
-        group = "tailscale-certs";
-      };
-    };
-
     services.tailscale = {
       enable = true;
       useRoutingFeatures = config.common.tailscale.role;
@@ -42,25 +26,5 @@
       lib.mkIf config.desktop.enable [
         tailscale-systray
       ];
-
-    systemd.services.tailscale-cert = let
-      domain = "${config.networking.hostName}.tailb76493.ts.net";
-      certPath = "/var/lib/tailscale/certs/${domain}.crt";
-      keyPath = "/var/lib/tailscale/certs/${domain}.key";
-    in
-      lib.mkIf (config.common.tailscale.role == "server" || config.common.tailscale.role == "both") {
-        description = "Generate Tailscale certs";
-        after = ["tailscaled.service"];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = [
-            "${pkgs.coreutils}/bin/mkdir -p /var/lib/tailscale/certs"
-            "${pkgs.tailscale}/bin/tailscale cert --cert-file ${certPath} --key-file ${keyPath} ${domain}"
-            "${pkgs.coreutils}/bin/chgrp tailscale-certs ${certPath} ${keyPath}"
-            "${pkgs.coreutils}/bin/chmod 640 ${certPath} ${keyPath}"
-          ];
-        };
-        wantedBy = ["multi-user.target"];
-      };
   };
 }
--- nixos/profiles/server/caddy.nix
+++ nixos/profiles/server/caddy.nix
@@ -4,8 +4,6 @@
   ...
 }: let
   domain = "${config.networking.hostName}.tailb76493.ts.net";
-  certPath = "/var/lib/tailscale/certs/${domain}.crt";
-  keyPath = "/var/lib/tailscale/certs/${domain}.key";
 in {
   options.server.caddy = {
     enable = lib.mkEnableOption "enable Caddy";
@@ -16,35 +14,23 @@ in {
       enable = true;
       virtualHosts."${domain}" = {
         extraConfig = ''
-          tls ${certPath} ${keyPath}
+          tls {
+            get_certificate tailscale
+          }
 
-          ${lib.optionalString config.server.immich.enable ''
-            handle /immich* {
-              reverse_proxy http://[::1]:${toString config.services.immich.port}
-            }
-
-            # root to immich
+          redir /immich /immich/
+          handle_path /immich/* {
             reverse_proxy http://[::1]:${toString config.services.immich.port}
-          ''}
+          }
         '';
       };
     };
 
     systemd.services.caddy = {
-      wants = ["tailscale-cert.service"];
-      after = ["tailscale-cert.service"];
       serviceConfig = {
         Group = "caddy";
-        SupplementaryGroups = ["tailscale-certs"];
-        ReadOnlyPaths = [
-          "/var/lib/tailscale/certs"
-          certPath
-          keyPath
-        ];
       };
     };
-
-    users.users.caddy.extraGroups = ["tailscale-certs"];
 
     networking.firewall.allowedTCPPorts = [80 443];
   };
