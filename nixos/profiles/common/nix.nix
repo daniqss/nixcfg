8c7bac9261d0f616285798dd477e330a1c139c0c
daniqss <danielqueijo14@gmail.com>
2026-05-13 11:50:46 +0200

    fix caches

--- nixos/profiles/common/nix.nix
+++ nixos/profiles/common/nix.nix
@@ -10,13 +10,13 @@
       download-buffer-size = 524288000;
       experimental-features = ["nix-command" "flakes"];
 
-      trusted-substituters = [
+      extra-substituters = [
         "https://nixos-raspberrypi.cachix.org"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
         "https://vicinae.cachix.org"
       ];
-      trusted-public-keys = [
+      extra-trusted-public-keys = [
         "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
